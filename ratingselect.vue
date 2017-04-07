40b682d54a00f1a4908aa22a11dac43c50629736
xiaochenggit <707996290@qq.com>
2017-04-07 16:14:47 +0800

    rt
    rt

---
+++ ratingselect.vue
@@ -0,0 +1,143 @@
+<template>
+	<div class="ratingselect">
+		<div class="rating-type">
+			<span @click="select(2,$event)" class="block positive" :class="{'active':selectType===2}">{{desc.all}}<span class="count">{{ratings.length}}</span></span>
+			<span @click="select(0,$event)" class="block positive" :class="{'active':selectType===0}">{{desc.positive}}<span class="count">{{positives}}</span></span>
+			<span @click="select(1,$event)" class="block negative" :class="{'active':selectType===1}">{{desc.negative}}<span class="count"></span></span>
+		</div>
+		<div @click="toggleContent" class="switch" :class="{'on':onlyContent}">
+			<span class="icon-check_circle"></span>
+			<span class="text">只看有内容的评价</span>
+		</div>
+	</div>
+</template>
+<script>
+const POSITIVE = 0;
+const NEGATIVE = 1;
+const ALL = 2;
+export default {
+	props: {
+		ratings: {
+			type: Array,
+			default() {
+				return [];
+			}
+		},
+		selectType: {
+			type: Number,
+			default: ALL
+
+		},
+		onlyContent: {
+			type: Boolean,
+			default: false
+		},
+		desc: {
+			type: Object,
+			default: function() {
+				return {
+					all: '全部',
+					positive: '满意',
+					negative: '不满意'
+				};
+			}
+		}
+	},
+	computed: {
+		positives: function () {
+			let positiveslength = [];
+			this.ratings.forEach(function(rating) {
+				if (rating.rateType === POSITIVE) {
+					rating.push(positiveslength);
+				}
+			});
+			return positiveslength.length;
+		},
+        netatives: function () {
+			let netativeslength = [];
+			this.ratings.forEach(function(rating) {
+				if (rating.rateType === NEGATIVE) {
+					rating.push(netativeslength);
+				}
+			});
+			return netativeslength.length;
+		}
+	},
+	methods: {
+		select: function(type, event) {
+			if (!event._constructed) {
+				return;
+			}
+			this.selectType = type;
+			this.$emit('ratingtype.select', type);
+		},
+		toggleContent: function(event) {
+			if (!event._constructed) {
+				return;
+			}
+			this.onlyContent = !this.onlyContent;
+			this.$emit('content.toggle', this.onlyContent);
+		}
+	}
+};
+</script>
+<style>
+.ratingselect{
+
+}
+.ratingselect .rating-type{
+	padding: 18px 0;
+	margin: 0 18px;
+	border-bottom: 1px solid rgba(7,17,27,0.1);
+	font-size: 0;
+}
+.ratingselect .rating-type .block{
+	display: inline-block;
+	padding: 8px 12px;
+	margin-right: 8px;
+	border-radius: 1px;
+	font-size: 12px;
+	color: rgb(77,85,93);
+	line-height: 16px;
+}
+.ratingselect .rating-type .block.active{
+	color: #fff;
+}
+.ratingselect .rating-type .block .count{
+	font-size: 8px;
+	margin-left: 2px;
+}
+.ratingselect .rating-type .positive{
+    background: rgba(0,160,220,0.2);    
+}
+.ratingselect .rating-type .positive.active{
+	background: rgb(0,160,220);
+}
+.ratingselect .rating-type .negative{
+	background: rgba(77,85,93,0.2);
+}
+.ratingselect .rating-type .negative.active{
+	background: rgb(77,85,93);
+}
+.switch{
+    padding:12px 18px;
+    line-height: 24px;
+    border-bottom: 1px solid rgba(7,17,27,0.1);
+    color: rgb(147,153,159);
+    font-size: 0;
+}
+.switch.on .icon-check_circle{
+	color: #00c850;
+}
+.switch .icon-check_circle{
+	display: inline-block;
+	vertical-align: top;
+	margin-right: 4px;
+    font-size: 24px;
+}
+.switch .text{
+	display: inline-block;
+	vertical-align: top;
+    font-size: 12px;
+}
+</style>
